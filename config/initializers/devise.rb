96f6f70cd4bc9f27ebc2d35f31114651fa4737c7
dev.umigo <dev.umigo@gmail.com>
2019-07-07 11:30:37 +0300

    added facebook sign in button to new.html.erb in devise sessions

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -296,4 +296,10 @@ Devise.setup do |config|
   # When set to false, does not sign a user in automatically after their password is
   # changed. Defaults to true, so a user is signed in automatically after changing a password.
   # config.sign_in_after_change_password = true
+
+  config.omniauth :facebook, "329887951299865", "1f867cadadd0f4f2d53d4721bd75395b", 
+                  scope: 'email',
+                  info_fields: 'email,name',
+                  image_size: 'large'
+
 end
