a4bd64c1e3bea90b16fbaf77e7b732f67e227dfa
dev.umigo <dev.umigo@gmail.com>
2019-07-07 17:58:32 +0300

    updated application.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,7 +14,7 @@ $(document).ready(() => {
   $(".toggle").on("click", e => {
     e.stopPropagation();
     e.preventDefault();
-    $("#" + e.target.getAttribute("aria-controls"));
+    $("#" + e.target.getAttribute("aria-controls")).toggleClass("is-hidden");
   });
 });
 
