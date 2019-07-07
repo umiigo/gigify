8fd755c54ef2b3b102e0f8348b496f7780155337
dev.umigo <dev.umigo@gmail.com>
2019-07-07 12:26:33 +0300

    registrations controller added to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   devise_for :users, 
               path: '',
               path_names:{sign_up: 'register', sign_in: 'login', edit: 'profile', sign_out:'logout'},
-              controllers: {omniauth_callbacks: 'omniauth_callbacks'}
+              controllers: {omniauth_callbacks: 'omniauth_callbacks', registrations: 'registrations'}
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
