e80ef9e29f9a0c9b4f3707fb422ec154bcd69ae0
dev.umigo <dev.umigo@gmail.com>
2019-07-08 11:42:01 +0300

    made migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_07_150245) do
+ActiveRecord::Schema.define(version: 2019_07_08_083838) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,37 @@ ActiveRecord::Schema.define(version: 2019_07_07_150245) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "gigs", force: :cascade do |t|
+    t.string "title"
+    t.string "video"
+    t.boolean "active"
+    t.boolean "has_single_pricing"
+    t.bigint "user_id", null: false
+    t.bigint "category_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["category_id"], name: "index_gigs_on_category_id"
+    t.index ["user_id"], name: "index_gigs_on_user_id"
+  end
+
+  create_table "pricings", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.integer "delivery_time"
+    t.integer "price"
+    t.integer "pricing_type"
+    t.bigint "gig_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["gig_id"], name: "index_pricings_on_gig_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -57,4 +88,7 @@ ActiveRecord::Schema.define(version: 2019_07_07_150245) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "gigs", "categories"
+  add_foreign_key "gigs", "users"
+  add_foreign_key "pricings", "gigs"
 end
