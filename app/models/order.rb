5de01a6ca5dee477c0ca03fd951058948dded944
dev.umigo <dev.umigo@gmail.com>
2019-08-05 11:36:39 +0100

    set up order model

---
+++ app/models/order.rb
@@ -0,0 +1,9 @@
+class Order < ApplicationRecord
+  belongs_to :gig, required: false
+
+  belongs_to :buyer, class_name: "User",
+  belongs_to :seller, class_name: "User",
+
+  enum status: [:inprogress, :completed ]
+
+end
